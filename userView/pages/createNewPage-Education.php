c63f50e1eba3d1a9a57b094aeb692988999a51eb
Marcos DeSousa <contact.mdesousa@gmail.com>
2016-02-17 09:30:05 -0500

    creating new pages tutorial
    creating new pages tutorial

---
+++ userView/pages/createNewPage-Education.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Marriott Vacation Club Web Dev | Components</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="/IDS-Toolbox/userView/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
+    <!-- FontAwesome 4.3.0 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons 2.0.0 -->
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
+    <!-- Theme style -->
+    <link href="/IDS-Toolbox/userView/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. Choose a skin from the css/skins 
+         folder instead of downloading all of them to reduce the load. -->
+    <link href="/IDS-Toolbox/userView/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="/IDS-Toolbox/userView/plugins/iCheck/flat/blue.css" rel="stylesheet" type="text/css" />
+    
+    <!-- DATA TABLES -->
+    <link href="/IDS-Toolbox/userView/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+
+    <!-- fullCalendar 2.2.5-->
+    <link href="/IDS-Toolbox/userView/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
+    <link href="/IDS-Toolbox/userView/plugins/fullcalendar/fullcalendar.print.css" rel="stylesheet" type="text/css" media='print' />
+    
+    
+    <!-- PrismJS.com -->
+    <link href="/IDS-Toolbox/userView/dist/css/prism.css" rel="stylesheet" /> 
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <!--
+  BODY TAG OPTIONS:
+  =================
+  Apply one or more of the following classes to get the
+  desired effect
+  |---------------------------------------------------------|
+  | SKINS         | skin-blue                               |
+  |               | skin-black                              |
+  |               | skin-purple                             |
+  |               | skin-yellow                             |
+  |               | skin-red                                |
+  |               | skin-green                              |
+  |---------------------------------------------------------|
+  |LAYOUT OPTIONS | fixed                                   |
+  |               | layout-boxed                            |
+  |               | layout-top-nav                          |
+  |               | sidebar-collapse                        |
+  |               | sidebar-mini                            |
+  |---------------------------------------------------------|
+  -->
+  <body class="skin-blue sidebar-mini">
+    <div class="wrapper">
+
+		<?php require_once('../header.php'); ?>
+		<?php require_once('../sidebar.php'); ?>
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Creating New Page - Education
+            <small>How-To: How to create a new page in Hybris.</small>
+          </h1>
+          <?php
+          $parentPage = "Coding Standards";
+          $pageName = "Forms";
+          ?>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active"><a href="#"><?php echo $parentPage; ?></a></li>
+            <li><a href="#"><?php echo $pageName; ?></a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+			<!-- START PAGE -->
+			<h2 class="page-header">MVC Forms</h2>
+			
+			<!-- START ROW -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="col-md-6">
+						<div class="box box-solid">
+							<div class="box-header with-border">
+								<h3 class="box-title">HTML Forms</h3>
+							</div><!-- /.box-header -->
+							
+							<div class="box-body">
+								test
+
+							</div><!-- /.box-body -->
+						</div><!-- /.box -->
+					</div>
+					
+					<div class="col-md-6">
+						<div class="box box-solid">
+							<div class="box-header with-border">
+								<h3 class="box-title">HTML Forms</h3>
+							</div><!-- /.box-header -->
+							
+							<div class="box-body">
+							<video>
+								<source src="videos/tutorials/educationPage-createNewPageExample1.mp4" type="video/mp4">
+								<source src="videos/tutorials/educationPage-createNewPageExample.webm" type="video/webm">
+								<source src="videos/tutorials/educationPage-createNewPageExample.ogg" type="video/ogg">
+							</video>
+
+							</div><!-- /.box-body -->
+						</div><!-- /.box -->
+					</div>
+				</div>
+			</div>
+			<!-- END CONTENT -->
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+      
+
+    <!-- PrismJS.com -->
+    <script src="/dist/js/prism.js"></script>   
+		<?php require_once('../footer.php'); ?>
+		
+    
+    
