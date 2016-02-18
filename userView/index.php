351a4c5fca3aba935d01f335aa3dd722464d3037
Marcos DeSousa <contact.mdesousa@gmail.com>
2016-02-18 15:14:59 -0500

    remove test word

--- userView/index.php
+++ userView/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>IDS Toolbox | Dashboard test</title>
+    <title>IDS Toolbox | Dashboard</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link  href="/IDS-Toolbox/userView/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
